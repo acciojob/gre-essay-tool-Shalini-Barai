ff4c12869ab144c88aeb8b1f1ebfc42c12523ab8
Shalini-Barai <115135802+Shalini-Barai@users.noreply.github.com>
2022-12-27 10:49:22 +0530

    gre-essay1

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 let etext=document.getElementById("evaluatedText");
 let countw=document.getElementById("wordCount");
+
 var c=0;
-etext.addEventListener("input",()=>{
-   c++;
-   countw.innerHTML=c;
+/*etext.addEventListener("input",()=>{
+        c++;
+        countw.innerHTML=c;
+    }
+)*/
+etext.addEventListener("keydown",(e)=>{
+    if(e.key!='Backspace'){
+    c++;
+    countw.innerHTML=c;
+    }
+    else{
+        c--;
+        countw.innerHTML=c;
+
+    }
+
 })
