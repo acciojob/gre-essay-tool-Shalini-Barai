1f284977113a5a2d25a427f7ab6c9bb65b246d2b
Shalini-Barai <115135802+Shalini-Barai@users.noreply.github.com>
2022-12-27 10:03:44 +0530

    gre-essay

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your code here
+let etext=document.getElementById("evaluatedText");
+let countw=document.getElementById("wordCount");
+var c=0;
+etext.addEventListener("input",()=>{
+   c++;
+   countw.innerHTML=c;
+})
